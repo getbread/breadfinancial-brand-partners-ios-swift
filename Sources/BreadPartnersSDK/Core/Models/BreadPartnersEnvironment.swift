f40e3406077d0616cb5949f540ff4c4cdb945b45
SINGHAL, SHUBHAM <SHUBHAM.SINGHAL@breadfinancial.com>
2025-03-27 20:39:39 +0530

    Added Bread Financial license headers to all Swift files

--- Sources/BreadPartnersSDK/Core/Models/BreadPartnersEnvironment.swift
+++ Sources/BreadPartnersSDK/Core/Models/BreadPartnersEnvironment.swift
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------------
+//  File:          BreadPartnersEnvironment.swift
+//  Author(s):     Bread Financial
+//  Date:          27 March 2025
+//
+//  Descriptions:  This file is part of the BreadPartnersSDK for iOS,
+//  providing UI components and functionalities to integrate Bread Financial
+//  services into partner applications.
+//
+//  © 2025 Bread Financial
+//------------------------------------------------------------------------------
+
 /// Represents the different environments the SDK can operate in.
 ///
 /// - `stage`: Use this environment for testing and development.
--- Sources/BreadPartnersSDK/Utilities/BreadPartnerDefaults.swift
+++ Sources/BreadPartnersSDK/Utilities/BreadPartnerDefaults.swift
@@ -1,3 +1,15 @@
+//------------------------------------------------------------------------------
+//  File:          BreadPartnerDefaults.swift
+//  Author(s):     Bread Financial
+//  Date:          27 March 2025
+//
+//  Descriptions:  This file is part of the BreadPartnersSDK for iOS,
+//  providing UI components and functionalities to integrate Bread Financial
+//  services into partner applications.
+//
+//  © 2025 Bread Financial
+//------------------------------------------------------------------------------
+
 import UIKit
 
 /// `BreadPartnerDefaults` class provides default configurations/styles/properties used across the BreadPartner SDK.
