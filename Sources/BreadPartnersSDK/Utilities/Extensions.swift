a6f775266187106533971b9eebc27be1eb7b9c59
Joncarlos Tavarez <joncarlos.tavarez@breadfinancial.com>
2026-08-17 16:29:06 -0400

    making default text and button colors to black (#30)

--- Sources/BreadPartnersSDK/Utilities/Extensions.swift
+++ Sources/BreadPartnersSDK/Utilities/Extensions.swift
@@ -52,11 +52,7 @@ extension UILabel {
         if let font = style.font {
             self.font = font
         }
-        
-        if let textColor = style.textColor {
-            self.textColor = textColor
-        }
-
+        self.textColor = style.textColor
     }
 }
 
